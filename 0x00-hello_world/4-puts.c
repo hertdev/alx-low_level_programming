1bb34dcf96400a6e434496d2335a1e25bf57f01e
phosecode <victorolamy@gmail.com>
2023-05-20 13:24:21 -0400

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-/* 4-puts.c - A program that prints */
+/**
+  4-puts.c - A program that prints
+  main - Entry point
+
+  Return: Always 0 (success)
+ */
 int main(void)
 {
 	printf("\"Programming is like building a multilingual puzzle\n");
