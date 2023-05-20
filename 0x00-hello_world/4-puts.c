814cb11f4e7ee78777ecad09e77c51bbbe172def
phosecode <victorolamy@gmail.com>
2023-05-20 13:18:19 -0400

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/* puts - A program that prints */
+/* _4-puts.c - A program that prints */
 int main(void)
 {
 	printf("\"Programming is like building a multilingual puzzle\n");
