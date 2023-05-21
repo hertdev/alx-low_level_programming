4e344a55403fbc1dcce3cd9a5ae6396d3c892773
phosecode <victorolamy@gmail.com>
2023-05-21 14:02:25 -0400

    project 3 commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  * main - Program entry point
+  * 2-print_alphabet - Prints alpha in lowercase
+  *
+  *Return: Always Success (0)
+  */
+int main(void)
+{
+	char low = 'a';
+
+	while (low <= 'z')
+	{
+		printf("%c", low);
+		low++;
+	}
+	return (0);
+}
