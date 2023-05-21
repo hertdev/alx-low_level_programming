68d71b08c7503c388268619f03664ebd40e8de4f
phosecode <victorolamy@gmail.com>
2023-05-21 16:34:49 -0400

    project 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,7 +9,14 @@ int main(void)
 	int num;
 
 	for (num = 0; num < 10; num++)
-		printf("%d, ", num);
-	printf("\n");
+	{
+		putchar(num);
+
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	putchar('\n');
 	return (0);
 }
