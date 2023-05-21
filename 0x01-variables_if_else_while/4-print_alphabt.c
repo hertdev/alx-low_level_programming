fe7321fc296d6ca17ffd6f4c986b9a997c137ccb
phosecode <victorolamy@gmail.com>
2023-05-21 15:22:50 -0400

    project 4 commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+  * main - Program entry point
+  * 4-print_alphabt - program printing alphabet
+  *
+  *Return: letters of alphabet except e and q
+  */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if (alpha != 'e' && alpha != 'q')
+			putchar(alpha);
+		else
+			NULL;
+	}
+	putchar("\n");
+	return (0);
+}
