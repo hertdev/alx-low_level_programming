a0f49c0fd64701d35429dd00db45b420705541ee
phosecode <victorolamy@gmail.com>
2023-05-21 16:11:41 -0400

    project 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+  * main - Program entry point
+  * 8-print_base16 - program printing alphabet
+  *
+  *Return: numbers in base 16
+  */
+int main(void)
+{
+	int num = 0;
+	char alpha = 'a';
+
+	while (num < 10)
+	{
+		putchar(num);
+		num++;
+	}
+	while (alpha <= 'f')
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	putchar('\n');
+	return (0);
+}
