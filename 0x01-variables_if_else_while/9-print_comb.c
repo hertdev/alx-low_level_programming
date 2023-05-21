5bbee26b558f068f9f0576624ce3c1acc9710ae6
phosecode <victorolamy@gmail.com>
2023-05-21 16:29:48 -0400

    project 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+  * main - Program entry point
+  * 9-print_comb - print number in ascending order
+  * Return: Always success (0)
+  */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d, ", num);
+	printf("\n");
+	return (0);
+}
