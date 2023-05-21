2266e27a7a455437db268ee75ab66c2ee16f8ade
phosecode <victorolamy@gmail.com>
2023-05-21 15:31:44 -0400

    project 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  * main - Program entry point
+  * 5-print_numbers - prints numbers in base ten
+  *
+  * Return: numbers in base 10
+  */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+		printf("%d", number);
+	printf("\n");
+	return (0);
+}
