dbc8b4b2d7e55d884e4076a41d1c24178ff06767
phosecode <victorolamy@gmail.com>
2023-05-22 05:00:50 -0400

    project 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+  * main - Program entry point
+  * 102-print_comb5 - print range of numbers
+  * Return: Always Success (0)
+  */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = 0; j < 100; j++)
+		{
+			if (i < j)
+			{
+				putchar((i / 10) + 48);
+				putchar((j / 10) + 48);
+				putchar(' ');
+				putchar((i % 10) + 48);
+				putchar((j % 10) + 48);
+
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
