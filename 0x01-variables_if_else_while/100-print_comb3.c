f1523b53ae30d3ab569d6995661bcd5dfcde6200
phosecode <victorolamy@gmail.com>
2023-05-22 00:02:43 -0400

    project 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of two digits
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b;
+
+	for ((a = 48; a <= 56; a++) && (b = 49; b <= 57; b++))
+	{
+		if (a > b)
+		{
+			putchar(a);
+			putchar(b);
+
+			if (a != 56 || b != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
