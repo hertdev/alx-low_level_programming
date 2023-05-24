48724b6dcf92e2044e4ce7b2887e2d4e808affcb
Victor Ogunniran <ogunniranolaoluwa12@gmail.com>
2023-05-24 10:00:06 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,16 +10,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		return (n);
+		return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar(0);
-		return (n);
+		_putchar(48);
+		return (0);
 	}
 	else if (n < 0)
 	{
 		_putchar('-');
-		return (n);
+		return (-1);
 	}
 }
