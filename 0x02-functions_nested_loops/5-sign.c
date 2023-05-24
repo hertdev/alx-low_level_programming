319b80f7a0cc097bb7e43bdb71e4f8f585ff0ea8
phosecode <victorolamy@gmail.com>
2023-05-24 04:46:43 -0400

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * print_sign - program that print sign of a number
+  * @n: function parameter integer
+  * Return: signs
+  */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+		return (NULL);
+}
