ddd80938401060a78c522c988ae72173a35b5dcb
phosecode <victorolamy@gmail.com>
2023-05-24 22:49:49 -0400

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * _abs - Prints the absolute value of an integer
+  * @int: Integer value to consider
+  * Return: absolute value
+  */
+int _abs(int)
+{
+	int n;
+
+	if (n < 0)
+		n = n * (-1);
+	_putchar(n);
+	return (0);
+}
