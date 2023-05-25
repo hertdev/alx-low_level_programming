3fbd6aa90da8930a631f6ab57feca55f50810293
phosecode <victorolamy@gmail.com>
2023-05-25 04:26:21 -0400

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+  * _isupper - check for uppercase
+  * @c: Integer to consider
+  * Return: Always success (0)
+  */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+# 0x04. C - More functions, more nested loops
+Just more on Function and nested loop. Thanks
