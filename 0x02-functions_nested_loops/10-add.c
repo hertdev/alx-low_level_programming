bcef6c9d8ca03fb8d4e8c92fa405afa12f25026a
phosecode <victorolamy@gmail.com>
2023-05-25 00:45:02 -0400

    10 add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * add - function that add two integer
+  * @a: int param
+  * @b: int param 2
+  * Return: result of addition
+  */
+int add(int a, int b)
+{
+	return (a + b);
+}
