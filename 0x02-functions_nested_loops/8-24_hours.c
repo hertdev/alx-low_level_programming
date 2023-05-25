29da82fffcc33403725f96362f437e9faaf144b6
phosecode <victorolamy@gmail.com>
2023-05-24 23:56:18 -0400

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * jack_bauer - prints every minute of the day
+  * Return: time from 00:00 to 23:59
+  */
+void jack_bauer(void)
+{
+	int hours, mins;
+
+	for (hours = 0; hours < 24; hours++)
+	{
+		for (mins = 0; mins < 60; mins++)
+		{
+			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0');
+			_putchar(':');
+			_putchar((mins / 10) + '0');
+			_putchar((mins % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
