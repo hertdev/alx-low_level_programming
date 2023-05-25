16ae02aab28633521957cc423521fd12bd0fb241
phosecode <victorolamy@gmail.com>
2023-05-24 23:22:14 -0400

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * print_last_digit - computes the last digit of a number
+  * @n: integer to consider
+  * Return: the last digit of a number
+  */
+int print_last_digit(int n)
+{
+	int print_num;
+
+	print_num = n % 10;
+	return (print_num);
+}
