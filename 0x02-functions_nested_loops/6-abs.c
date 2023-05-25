d2baae5f26c4e7f726de54a43a28d9a97f2bbebf
phosecode <victorolamy@gmail.com>
2023-05-24 23:13:22 -0400

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -9,13 +9,8 @@
   */
 int _abs(int n)
 {
-<<<<<<< HEAD
-	if (n < 0)
-		n = n * (-);
-	_putchar(n);
-	return (0);
-=======
-	int x = abs(n);
+	int x;
+
+	x = abs(n);
 	return (x);
->>>>>>> 55e78890e3e6ef7aa98632ebab9c13a5d9c6c98b
 }
