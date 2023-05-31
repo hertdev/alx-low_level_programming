bc2979351e62dbc10513a855a47c9ecbfd4b9b86
phosecode <victorolamy@gmail.com>
2023-05-31 03:03:45 +0100

    8-array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * print_array - prints an array of number
+  * @a: pointer element
+  * @n: array number
+  * Return: array of numbers
+  */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != n - 1)
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+	}
+	_putchar('\n');
+}
