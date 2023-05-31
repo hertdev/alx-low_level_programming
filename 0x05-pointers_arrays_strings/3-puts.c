2a1b6938010ea2c4b71a125971d3742ad6022b2f
phosecode <victorolamy@gmail.com>
2023-05-31 10:32:56 +0100

    3-puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * _puts - print string to standard output
+  * @str: string pointer
+  * Retuirn: Print string to stdout
+  */
+void _puts(char *str)
+{
+	puts(str);
+}
