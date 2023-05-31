dab0baa51d8e5184659e33df353259ed541edc8b
phosecode <victorolamy@gmail.com>
2023-05-31 01:46:25 +0100

    100-atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,9 +8,13 @@
   */
 int _atoi(char *s)
 {
-	int result;
+	int num = 0;
 
-	result = atoi(s);
-
-	return (result);
+	for (int i = 0; s[i] != '\0'; i++)
+	{
+		num = num * 10 + (s[i] - 48);
+		_putchar(num);
+	}
+	_putchar('\n');
+	return (0);
 }
