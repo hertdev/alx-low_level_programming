06b2bc00bb87466e572b2fb297aa48db2b7061ad
phosecode <victorolamy@gmail.com>
2023-05-31 01:23:12 +0100

    100-atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * _atoi - convert a string to an integer
+  * @s: pointer to char
+  * Return: Integer
+  */
+int _atoi(char *s)
+{
+	int result;
+
+	result = atoi(s);
+
+	return (result);
+}
