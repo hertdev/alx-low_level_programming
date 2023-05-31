a003555ceb31c070aa5cab26364af16c8595469f
phosecode <victorolamy@gmail.com>
2023-05-31 10:15:35 +0100

    2-strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+
+/**
+  * _strlen - string length function
+  * @s: pointer to char
+  * Return: string length
+  */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
