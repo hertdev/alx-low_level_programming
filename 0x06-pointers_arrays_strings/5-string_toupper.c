0de730f99fec64dd25a9f5db73528b22f3bf7c3d
phosecode <victorolamy@gmail.com>
2023-06-02 05:05:26 +0100

    to uppercase task

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * string_toupper - converts all lowercase string to uppercase
+  * @s: modified string
+  * Return: uppercase string
+  */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; i != '\0'; i++)
+	{
+		if (s[i] > 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+	}
+	return (s);
+}
