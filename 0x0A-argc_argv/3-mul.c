a85855d41804a195871a76649d1b70eb2fb890f5
phosecode <victorolamy@gmail.com>
2023-06-13 07:11:11 +0100

    3-mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,40 +2,72 @@
 #include "main.h"
 
 /**
-  * myAtoi - converts string to integer
-  * @s: string pointer
-  * Return: converted string
-  */
-int myAtoi(char *s)
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+int _atoi(char *s)
 {
-	int res = 0;
-	int i;
+	int i, d, n, len, f, digit;
 
-	for (i = 0; s[i] != '\0'; i++)
-		res = res * 10 + s[i] - '0';
-	return (res);
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
 }
 
 /**
-  * main - program entry point
-  * @argc: argument count
-  * @argv: argument vector
-  * Return: multiplied number
-  */
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
 int main(int argc, char *argv[])
 {
-	int num, num1, result;
+	int result, num1, num2;
 
 	if (argc < 3 || argc > 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else
-	{
-		num = myAtoi(argv[1]);
-		num1 = myAtoi(argv[2]);
-		result = num * num1;
-		printf("%d\n", result);
-	}
+
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+
+	return (0);
 }
