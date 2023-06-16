c2a607a33ee893b9fd87a884f77b88b228e7bb7d
phosecode <victorolamy@gmail.com>
2023-06-16 02:53:14 +0100

    malloc project 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -24,15 +24,15 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 
 	old_ptr = ptr;
-
 	new_ptr = malloc(new_size);
-
 	if (!new_ptr)
 		return (NULL);
+
 	if ((new_size > old_size) || (new_size < old_size))
 	{
 		for (i = 0; ((i < new_size) && (i < old_size)); i++)
 			new_ptr[i] = old_ptr[i];
 	}
+	free(ptr);
 	return (new_ptr);
 }
