71a7ee559e1315ea63092824b3b0593c6d4e1b63
phosecode <victorolamy@gmail.com>
2023-06-16 02:34:10 +0100

    malloc project 4

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+  * _realloc - reallocate memory block
+  * @ptr: pointer to memory
+  * @old_size: old memory size
+  * @new_size: new memory size
+  * Return: array of reallocated memory
+  */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *new_ptr;
+	char *old_ptr;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (!ptr)
+		return (malloc(new_size));
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	old_ptr = ptr;
+
+	new_ptr = malloc(new_size);
+
+	for (i = 0; ((new_size < old_size) && (new_size > old_size)); i++)
+		new_ptr[i] = old_ptr[i];
+	return (new_ptr);
+}
