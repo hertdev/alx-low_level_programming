76838e953ac81de8b3e3868eb25b76909b826744
phosecode <victorolamy@gmail.com>
2023-06-20 21:08:00 +0100

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,11 @@
+#include "main.h"
+#include <stdio.h>
 /**
   * main - program entry
   * Return: name of file the program is compile from
   */
 int main(void)
 {
-	_putchar(__FILE__);
+	printf("%s\n",__FILE__);
 	return (0);
 }
