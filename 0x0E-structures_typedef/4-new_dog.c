c11c46b75330fb9e2c1443d0ace22a124939ea4d
phosecode <victorolamy@gmail.com>
2023-06-21 05:45:48 +0100

    new_dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,42 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+  * new_dog - pointer to dog_t
+  * @name: new dog name
+  * @age: new dog age
+  * @owner: new dog owner
+  * Return: values of new dog
+  */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog *dog;
+	int len1, len2;
+
+	len1 = strlen(name);
+	len2 = strlen(owner);
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	dog->name = malloc(sizeof(char) * len1 + 1);
+	if (dog->name == NULL)
+	{
+		return (NULL);
+		free(dog->name);
+	}
+	dog->owner = malloc(sizeof(char) * len2 + 1);
+
+	if (dog->owner == NULL)
+	{
+		return (NULL);
+		free(dog->owner);
+	}
+	strcpy(dog->name, name);
+	strcpy(dog->owner, owner);
+	dog->age = age;
+
+	return (dog);
+	free(dog);
+}
