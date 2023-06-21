1829df787037430947466ce1ca899f041d64ee74
phosecode <victorolamy@gmail.com>
2023-06-21 04:15:05 +0100

    dog init

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+  * init_dog - initializes dog variable type
+  * @d: dog structure
+  * @name: dog name
+  * @age: dog age
+  * @owner: dog owner
+  */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
