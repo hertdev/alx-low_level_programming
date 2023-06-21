bf022f9dc10b3fe02ff4ad35948ae9014a9ff2ec
phosecode <victorolamy@gmail.com>
2023-06-21 03:48:57 +0100

    dog.h task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - dog animal data
+  * @name: name of the dog
+  * @age: age of the dog
+  * @owner: Owner of the dog
+  */
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+}
+
+#endif
