71993176891aebfdcadeff432da25ba12ef7c6bf
phosecode <victorolamy@gmail.com>
2023-06-26 06:55:02 +0100

    array index search

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+  * int_index - function that searches for an integer
+  * @array: array of elements
+  * @size: size of array
+  * @cmp: the function pointer
+  *
+  * Return: void integer search result
+  */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int indx;
+
+	if (cmp && array)
+	{
+		for (indx = 0; indx < size; indx++)
+		{
+			if (cmp(array[indx]) != 0)
+				return (indx);
+		}
+	}
+	
+	return (-1);
+}
