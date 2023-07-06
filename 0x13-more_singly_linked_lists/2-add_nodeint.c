a8b8594af31aaba551a51ed1f7719b49d9df4e02
phosecode <victorolamy@gmail.com>
2023-07-06 02:00:11 +0100

    Print node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -12,9 +12,9 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *new;
 
 	new = malloc(sizeof(listint_t));
-	if (new == NULL)
+	if (!new)
 	{
-		printf("Error\n");
+		printf("NULL");
 	}
 
 	new->n = n;
