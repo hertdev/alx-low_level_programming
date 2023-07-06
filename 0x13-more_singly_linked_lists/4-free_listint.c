133be532b00a579d73cc93c3e0ece4b5b5447a23
phosecode <victorolamy@gmail.com>
2023-07-06 03:21:22 +0100

    free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+  * free_listint - frees a list
+  * @head - head of list pointer
+  *
+  * Return: freed heap summary
+  */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	temp = head->next;
+	free(head);
+	head = temp;
+}
