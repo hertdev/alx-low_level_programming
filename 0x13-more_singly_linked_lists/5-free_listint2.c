c22e2bb2ea43ccece654a0a4ce000f238ed839bb
phosecode <victorolamy@gmail.com>
2023-07-06 03:50:08 +0100

    free list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+  * free_listint2 - frees list
+  * @head: head of list pointer
+  *
+  * Return: return head NULL
+  */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (*head)
+	{
+		temp = *head;
+		free(*head);
+		*head = temp;
+	}
+
+	*head = NULL;
+}
