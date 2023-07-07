54786922446eb9054922cf52e293d66dcd86508f
phosecode <victorolamy@gmail.com>
2023-07-07 03:23:59 +0100

    delete head node

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+  * program that delete the head node
+  * @head: head pointer
+  *
+  * Return: head node
+  */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int num;
+
+	if(!head || !*head)
+		return (0);
+
+	num = (*head)->n;
+	temp = (*head)->next;
+	free(*head);
+	*head = temp;
+
+	return (num);
+}
