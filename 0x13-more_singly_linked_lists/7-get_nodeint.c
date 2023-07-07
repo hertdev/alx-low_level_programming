56ff9534737192e90817a34c485b945e571e27db
phosecode <victorolamy@gmail.com>
2023-07-07 05:07:19 +0100

    node index :

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+  * get_nodeint_at_index - get node index
+  * @head: head node
+  * @index: node index
+  *
+  * Return: integer
+  */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *temp;
+
+	unsigned int i - 0;
+
+	while (temp && i < index)
+	{
+		temp = temp->next;
+		i++;
+	}
+	return (temp ? temp : NULL);
+}
