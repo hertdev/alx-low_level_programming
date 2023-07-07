353366efe53b57411fe2d89a3592b8cf8ef1e153
phosecode <victorolamy@gmail.com>
2023-07-07 23:44:52 +0100

    bit manipulation project 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * clear_bit - set the value of bit to 0 at index
+  * @n: bit pointer
+  * @index: bit index
+  *
+  * Return: set value
+  */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 64)
+		return (-1);
+	if ((*n >> index) & 1)
+	{
+		*n -= 1 << index;
+		return (1);
+	}
+	return (-1);
+}
