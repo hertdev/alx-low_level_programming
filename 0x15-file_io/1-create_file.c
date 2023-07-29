af2079ee1baad8eb8de6c4d4bfc484973d0d5376
trucedev <ogunniranolaoluwa12@gmail.com>
2023-07-29 18:11:20 +0100

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,8 +16,9 @@ int create_file(const char *filename, char *text_content)
 	op = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (op < 0)
 		return (-1);
-	if (text_content && text_content[len])
+	while (text_content && text_content[len])
 		len++;
+
 	err = write(op, text_content, len);
 	if (err < 0)
 		return (-1);
