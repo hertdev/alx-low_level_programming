8f7f9f045edf4117b42b488672033163770cddff
trucedev <ogunniranolaoluwa12@gmail.com>
2023-07-29 17:22:53 +0100

    create read file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+  * read_textfile - read text file and print it to standard output
+  * @filename: name of file
+  * @letters: number of letters to read and print
+  * Return: text to stdout
+  */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t, op, rd, wr;
+	char *buf;
+
+	if (!filename || !letters)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+		return (0);
+	op = open(filename, O_RDONLY);
+	rd = read(op, buf, letters);
+	wr = write(STDOUT_FILENO, buf, rd);
+
+	if (op == -1 || rd == -1 || wr == -1 || rd != wr)
+	{
+		free(buf);
+		return (0);
+	}
+	free(buf);
+	close(op);
+
+	return (wr);
+}
